5dd400219657b27a9523c172f0e8871efe96c3a7
boris <boris-K40IP@boris-K40IP-PC>
2017-06-24 11:59:07 +0800

    add eclips init
    Change-Id: If7496a495430229dc70c13f383a79e91a0c4c67c
    Signed-off-by: root <root@boris-K40IP>

---
+++ python/zlt.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+class Ant
+
+    def __init__(self,x=0,y=0,color='black'):
+        self.x = x
+        self.y = y
+        self.color = color
+        
+    def crwl(self,x,y):
+        self.x=x
+        self.y=y
+        print('paxing...')
+        
+    def info(self):
+        print ('dangqianweizhi:(%d,%d)' %(self.x,self.y))
+        
+    def attack(self):
+        print("yongcuiyao!")
+        
+class FlyAnt(Ant):
+    def attack(self):
+        print ("yongweizhen!")
+    def fly(self,x,y):
+        print ('feixing...')
+        self.x = x
+        self.y = y
+        self.info()
+flyant = FlyAnt(color = 'red') 
+flyant.craw(3,5)
+flyant.fly(10,14)
+flyant.attack()       
