8b67f1ba8680d81b9cf3323ac4d8aedd22a83be5
zlt <992592914@qq.com>
2017-07-20 17:56:34 +0800

    11

---
+++ python/py-scripts/ip.py
@@ -0,0 +1,10 @@
+#!bin/env python  
+#-*- coding: utf8 -*-  
+import os  
+#用只读的方式打开文件  
+f=open("./iplist.txt","r")  
+      
+#遍历文件f的每一行，并使用ping命令加上每一行的内容在command中执行  
+for line in f.readlines():  
+       os.system("ping "+line+" -c 2")  
+f.close()  
